879d16431fda2c75e1989f1a7f0c17f882084ed7
Blaze-trio <64141242+Blaze-trio@users.noreply.github.com>
2025-05-24 23:05:18 +0300

    feat: Add gh-pages deployment script and update package dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Minecraft-clone-Three.js/',
   plugins: [react()],
   server: {
     host: true,
